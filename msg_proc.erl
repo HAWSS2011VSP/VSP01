27dd967812adb38670e4ee19e14dc3a9b1fd4557
drexin <dario.rexin@r3-tech.de>
2011-04-20 07:49:29 +0200

    initial commit

---
+++ msg_proc.erl
@@ -0,0 +1,32 @@
+-module(msg_proc).
+-export([start/1]).
+
+
+start(Timeout) ->
+  spawn(fun() -> loop(Timeout) end).
+
+loop(Timeout) ->
+  receive
+    {waitForMsg, ID} ->
+      waitFor(ID, Timeout)
+  end.
+
+waitFor(ID, Timeout) ->
+  receive
+    {PID, putMessage, ID, Msg} ->
+      storage ! {PID, putMessage, ID, Msg}
+  after Timeout ->
+    storage ! {nil, putMessage, ID, lists:concat([">> Msg with ID ", ID, " is missing ", currentDateTime(), " <<"])}
+  end.
+
+currentDateTime() ->
+  {Year, Month, Day} = erlang:date(),
+  {Hour, Minute, Second} = erlang:time(),
+  lists:concat([mkString(".", [Day, Month, Year],[]), " ", mkString(":", [Hour, Minute, Second], [])]).
+
+mkString(Seperator, [], Result) ->
+  Result;
+mkString(Seperator, [Item|[]], Result) ->
+  mkString(Seperator, [], lists:concat([Result, Item]));
+mkString(Seperator, [Item|Rest], Result) ->
+  mkString(Seperator, Rest, lists:concat([Result, Item, Seperator])).
