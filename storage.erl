5209af7d395c0c1644c6f0794e4312e8f4b34363
drexin <dario.rexin@xing.com>
2011-04-20 23:06:11 +0200

    transfer to my imac ;-)

--- storage.erl
+++ storage.erl
@@ -2,12 +2,31 @@
 -export([start/1]).
 
 start(CheckInterval) ->
-  loop([],[], CheckInterval).
+  loop(dict:new(),dict:new(), 0, CheckInterval).
 
-loop(Clients, Messages, CheckInterval) ->
+loop(Clients, Messages, Offset, CheckInterval) ->
   receive
-    Some ->
-      loop(Clients, Messages, CheckInterval)
+    {PID, putMessage, ID, Msg} ->
+      {NewMsgs, NewOffset} = insertMessage(Messages, Msg, Offset),
+      loop(Clients, NewMsgs, NewOffset CheckInterval)
   after CheckInterval ->
-    loop(Clients, Messages, CheckInterval)
+    loop(Clients, Messages, Offset, CheckInterval)
   end.
+
+insertMessage(Msgs, Msg, Offset) ->
+  Size = dict:size(Msg),
+  if
+    Size >= 1000 ->
+      {dict:append(Size, Msg, dict:erase(Offset,Msgs)), Offset+1}
+    true ->
+      {dict:append(Size, Msg, Msgs), Offset}
+  end.
+  
+deleteIdleClients(Clients, Timeout) ->
+  deleteIdleClients(Clients, Timeout, []).
+  
+deleteIdleClients([], Timeout, Result) -> 
+  Result;
+deleteIdleClients([Client|Rest], Timeout, Result) ->
+  if
+    Client
